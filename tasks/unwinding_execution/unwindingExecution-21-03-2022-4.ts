870e4f60a78ffed4454fef1de8805d55a6904d89
Alexander Malyshev <alexanderem49@gmail.com>
2022-03-22 14:08:49 -0300

    Fix: scripts edited

--- tasks/unwinding_execution/unwindingExecution-21-03-2022-4.ts
+++ tasks/unwinding_execution/unwindingExecution-21-03-2022-4.ts
@@ -1,5 +1,6 @@
-import {task} from "hardhat/config";
+import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import { Unwinder } from "../../typechain";
 
 task("unwind", "Unwind entries from previous vote")
     .setAction(async function (taskArgs, hre) {
@@ -13,29 +14,29 @@ task("unwind", "Unwind entries from previous vote")
         const crv3 = "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490";
         const voteExecutor = "0x85adEF77325af70AC8922195fB6010ce5641d739";
         const gnosis = "0x1F020A4943EB57cd3b2213A66b355CB662Ea43C3";
-    
-        const exec = await hre.ethers.getContractAt("Unwinder", "0x0ccC76540E087b2E7567F7BFf80d7EEA0d4F00aC");
 
-        //Unwind the some of the mim position as per votes
+        const exec = await hre.ethers.getContractAt("Unwinder", "0x0ccC76540E087b2E7567F7BFf80d7EEA0d4F00aC") as Unwinder;
+
+        // Unwind the some of the mim position as per votes
         let entries = [
-            { 
-                weight: 38, 
-                entryToken: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-                curvePool: "0xCEAF7747579696A2F0bb206a14210e3c9e6fB269", 
-                poolToken: "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490", 
-                poolSize: 2, 
-                tokenIndexInCurve: 1, 
-                convexPoolAddress:"0xF403C135812408BFbE8713b5A23a04b3D48AAE31", 
-                convexPoold:59 
+            {
+                weight: 38,
+                entryToken: usdc,
+                curvePool: "0xCEAF7747579696A2F0bb206a14210e3c9e6fB269",
+                poolToken: crv3,
+                poolSize: 2,
+                tokenIndexInCurve: 1,
+                convexPoolAddress: "0xF403C135812408BFbE8713b5A23a04b3D48AAE31",
+                convexPoold: 59
             }
         ]
 
-        let unwindPercentage = 48
-        let outputCoin = usdc
-        let receiver = "0x85adEF77325af70AC8922195fB6010ce5641d739"
-        let swapRewards = true
+        let unwindPercentage = 48;
+        let outputCoin = usdc;
+        let receiver = voteExecutor;
+        let swapRewards = true;
 
-        await exec.unwindAny(entries, unwindPercentage, outputCoin, receiver, swapRewards)
+        await exec.unwindAny(entries, unwindPercentage, outputCoin, receiver, swapRewards);
 
         console.log('Unwinding task Done!');
     });
--- tasks/unwinding_execution/unwindingExecution-21-03-2022.ts
+++ tasks/unwinding_execution/unwindingExecution-21-03-2022.ts
@@ -14,29 +14,29 @@ task("unwind", "Unwind entries from previous vote")
         const crv3 = "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490";
         const voteExecutor = "0x85adEF77325af70AC8922195fB6010ce5641d739";
         const gnosis = "0x1F020A4943EB57cd3b2213A66b355CB662Ea43C3";
-    
-        const exec = await hre.ethers.getContractAt("Unwinder", "0x0ccC76540E087b2E7567F7BFf80d7EEA0d4F00aC");
 
-        //Unwind the whole d3Pool position as not in this week's vote
+        const exec = await hre.ethers.getContractAt("Unwinder", "0x0ccC76540E087b2E7567F7BFf80d7EEA0d4F00aC") as Unwinder;
+
+        // Unwind the whole d3Pool position as not in this week's vote
         let entries = [
-            { 
-                weight: 45, 
-                entryToken: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 
-                curvePool: "0xBaaa1F5DbA42C3389bDbc2c9D2dE134F5cD0Dc89", 
-                poolToken: "0x853d955aCEf822Db058eb8505911ED77F175b99e",
+            {
+                weight: 45,
+                entryToken: usdc,
+                curvePool: "0xBaaa1F5DbA42C3389bDbc2c9D2dE134F5cD0Dc89",
+                poolToken: frax,
                 poolSize: 3,
-                tokenIndexInCurve: 0, 
-                convexPoolAddress:"0xF403C135812408BFbE8713b5A23a04b3D48AAE31",
-                convexPoold:58
+                tokenIndexInCurve: 0,
+                convexPoolAddress: "0xF403C135812408BFbE8713b5A23a04b3D48AAE31",
+                convexPoold: 58
             }
-        ]
+        ];
 
-        let unwindPercentage = 100
-        let outputCoin = usdc
-        let receiver = "0x85adEF77325af70AC8922195fB6010ce5641d739"
-        let swapRewards = true
+        let unwindPercentage = 100;
+        let outputCoin = usdc;
+        let receiver = voteExecutor;
+        let swapRewards = true;
 
-        await exec.unwindAny(entries, unwindPercentage, outputCoin, receiver, swapRewards)
-        
+        await exec.unwindAny(entries, unwindPercentage, outputCoin, receiver, swapRewards);
+
         console.log('Unwinding task Done!');
     });
