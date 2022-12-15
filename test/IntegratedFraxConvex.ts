599707389e2a59eb04343b6f4507af48f61cd3b7
AliyaDav <aliya_dav98@mail.ru>
2022-12-15 10:16:33 +0100

    cleaning up integration tests

--- test/IntegratedFraxConvex.ts
+++ test/IntegratedFraxConvex.ts
@@ -1,10 +1,9 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { exec } from "child_process";
 import { BigNumber } from "ethers";
-import { AbiCoder, defaultAbiCoder, parseEther, parseUnits } from "ethers/lib/utils";
+import { parseEther, parseUnits } from "ethers/lib/utils";
 import { ethers, network, upgrades } from "hardhat";
-import { CurveFraxConvexStrategyV2, ICvxBooster, IERC20Metadata, IExchange, IWrappedEther, IConvexStaking, ILocking, IStrategyHandler, IVoteExecutorMaster, CurveConvexStrategyNativeV2 } from "../typechain";
+import { CurveFraxConvexStrategyV2, IERC20Metadata, IExchange, IStrategyHandler, IVoteExecutorMaster, IWrappedEther } from "../typechain";
 
 async function skipDays(d: number) {
     ethers.provider.send('evm_increaseTime', [d * 86400]);
@@ -27,22 +26,14 @@ async function getImpersonatedSigner(address: string): Promise<SignerWithAddress
 
 describe("Automated strategy execution", function () {
     let strategy: CurveFraxConvexStrategyV2;
-    let strategyNative: CurveConvexStrategyNativeV2;
     let signers: SignerWithAddress[];
     let signer: SignerWithAddress;
-
-    let usdc: IERC20Metadata, usdt: IERC20Metadata, crv: IERC20Metadata, cvx: IERC20Metadata,
-        FXS: IERC20Metadata, weth: IERC20Metadata, poolRewards: IERC20Metadata;
-    let cvxBooster: ICvxBooster;
     let exchange: IExchange;
-    let frax: IERC20Metadata;
-    let FraxPool: ILocking;
-    let stakingToken: IConvexStaking;
     let handler: IStrategyHandler;
     let executor: IVoteExecutorMaster;
+    let poolToken: IERC20Metadata;
 
     const duration = 60 * 60 * 24 * 8;  //EIGHT_DAYS_IN_SECONDS;
-
     const ZERO_ADDR = ethers.constants.AddressZero;
 
     async function resetNetwork() {
@@ -61,25 +52,9 @@ describe("Automated strategy execution", function () {
 
         signers = await ethers.getSigners();
         signer = signers[0]
-
-        usdc = await ethers.getContractAt("IERC20Metadata", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
-        usdt = await ethers.getContractAt("IERC20Metadata", "0xdAC17F958D2ee523a2206206994597C13D831ec7");
-        frax = await ethers.getContractAt('IERC20Metadata', '0x853d955acef822db058eb8505911ed77f175b99e');
-        FraxPool = await ethers.getContractAt('ILocking', '0x963f487796d54d2f27bA6F3Fbe91154cA103b199');
-        crv = await ethers.getContractAt("IERC20Metadata", "0xD533a949740bb3306d119CC777fa900bA034cd52");
-        cvx = await ethers.getContractAt("IERC20Metadata", "0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B");
-        FXS = await ethers.getContractAt("IERC20Metadata", "0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0");
-        weth = await ethers.getContractAt("IERC20Metadata", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
-        poolRewards = await ethers.getContractAt("IERC20Metadata", "0x7e880867363A7e321f5d260Cade2B0Bb2F717B02");
-        cvxBooster = await ethers.getContractAt("contracts/interfaces/ICvxBooster.sol:ICvxBooster", "0xF403C135812408BFbE8713b5A23a04b3D48AAE31") as ICvxBooster;
         exchange = await ethers.getContractAt("contracts/interfaces/IExchange.sol:IExchange", "0x29c66CF57a03d41Cfe6d9ecB6883aa0E2AbA21Ec") as IExchange
-        stakingToken = await ethers.getContractAt("IConvexStaking", "0x8a53ee42FB458D4897e15cc7dEa3F75D0F1c3475");
-
         const value = parseEther("200.0");
 
-        await exchange.exchange(
-            ZERO_ADDR, frax.address, value, 0, { value: value }
-        )
         await exchange.exchange(
             ZERO_ADDR, "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", value, 0, { value: value }
         )
@@ -100,10 +75,14 @@ describe("Automated strategy execution", function () {
         const curvePool = "0xDcEF968d416a41Cdac0ED8702fAC8128A64241A2";
         const fraxPool = "0x963f487796d54d2f27bA6F3Fbe91154cA103b199";
         const usdcToken = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-        const wethToken = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
         const poolSize = 2;
         const lpToken = '0x3175Df0976dFA876431C2E9eE6Bc45b65d3473CC';
         const tokenIndexInCurve = 1;
+
+        const _codeName = "Convex: FRAX/USDC";
+        const _entryToken = usdcToken;
+        const _assetId = 0;
+        const _chainId = 1;
 
         beforeEach(async () => {
 
@@ -120,24 +99,21 @@ describe("Automated strategy execution", function () {
             ) as CurveFraxConvexStrategyV2
 
             await strategy.grantRole("0x0000000000000000000000000000000000000000000000000000000000000000", "0x82e568c482df2c833dab0d38deb9fb01777a9e89");
+            handler = await ethers.getContractAt("IStrategyHandler", "0x385AB598E7DBF09951ba097741d2Fa573bDe94A5") as IStrategyHandler;
+            executor = await ethers.getContractAt("IVoteExecutorMaster", "0x82e568C482dF2C833dab0D38DeB9fb01777A9e89") as IVoteExecutorMaster;
+            poolToken = await ethers.getContractAt("IERC20Metadata", _entryToken);
 
         });
 
         it("Should add data to strategy handler ", async () => {
-            const _codeName = "Convex: FRAX/USDC";
-            const _strategyAddress = strategy.address;
-            const _entryToken = usdcToken;
-            const _assetId = 0;
-            const _chainId = 1;
             const _entryData = await strategy.encodeEntryParams(
                 curvePool, _entryToken, poolSize, tokenIndexInCurve, fraxPool, duration);
             const _rewardsData = await strategy.encodeRewardsParams(lpToken, fraxPool, 0);
             const _exitData = await strategy.encodeExitParams(curvePool, _entryToken, tokenIndexInCurve, fraxPool);
-            const handler = await ethers.getContractAt("IStrategyHandler", "0x385AB598E7DBF09951ba097741d2Fa573bDe94A5");
 
             await handler.addLiquidityDirection(
                 _codeName,
-                _strategyAddress,
+                strategy.address,
                 _entryToken,
                 _assetId,
                 _chainId,
@@ -147,7 +123,7 @@ describe("Automated strategy execution", function () {
             );
 
             const request = await handler.liquidityDirection(10);
-            expect(request.strategyAddress).to.be.eq(_strategyAddress);
+            expect(request.strategyAddress).to.be.eq(strategy.address);
             expect(request.entryToken).to.be.eq(_entryToken);
             expect(request.assetId).to.be.eq(_assetId);
             expect(request.chainId).to.be.eq(_chainId);
@@ -159,23 +135,15 @@ describe("Automated strategy execution", function () {
         });
 
         it("Should submit, approve and execute liquidity direction", async () => {
-            const _codeName = "Convex: FRAX/USDC";
-            const _strategyAddress = strategy.address;
-            const _entryToken = usdcToken;
-            const _assetId = 0;
-            const _chainId = 1;
+
             const _entryData = await strategy.encodeEntryParams(
                 curvePool, _entryToken, poolSize, tokenIndexInCurve, fraxPool, duration);
             const _rewardsData = await strategy.encodeRewardsParams(lpToken, fraxPool, 0);
             const _exitData = await strategy.encodeExitParams(curvePool, _entryToken, tokenIndexInCurve, fraxPool);
-            const handler = await ethers.getContractAt("IStrategyHandler", "0x385AB598E7DBF09951ba097741d2Fa573bDe94A5");
-            const executor = await ethers.getContractAt("IVoteExecutorMaster", "0x82e568C482dF2C833dab0D38DeB9fb01777A9e89");
-            const poolToken = await ethers.getContractAt("IERC20Metadata", _entryToken);
-            const amount = parseUnits("1000", await poolToken.decimals());
 
             await handler.addLiquidityDirection(
                 _codeName,
-                _strategyAddress,
+                strategy.address,
                 _entryToken,
                 _assetId,
                 _chainId,
@@ -199,42 +167,29 @@ describe("Automated strategy execution", function () {
 
             const admin = await getImpersonatedSigner('0x1F020A4943EB57cd3b2213A66b355CB662Ea43C3');
             await executor.connect(admin).setMinSigns(0);
+            const id = executor.submittedData.length;
+            console.log(id);
 
             await executor.executeSpecificData(3);
-            console.log('\n************* executed specific data *************\n');
             const executorBalanceAter = await poolToken.balanceOf(executor.address);
             console.log('Balance of executor before investing', ethers.utils.formatUnits(executorBalanceBefore, 6), "balance after withdrawal: ", ethers.utils.formatUnits(executorBalanceAter, 6), '\n**********************************');
 
             await executor.connect(admin).executeDeposits();
 
             const deployedAmount = ethers.utils.formatEther(await strategy.callStatic.getDeployedAmount(_rewardsData));
-            console.log(deployedAmount);
-            console.log('Balance of executor afer investing', ethers.utils.formatUnits((await poolToken.balanceOf(executor.address)), 6));
             checkSpread(ethers.utils.parseEther(deployedAmount), ethers.utils.parseEther(ethers.utils.formatUnits(executorBalanceBefore, 6)), 5);
-
-            // expect(executorBalanceAter).to.be.eq((executorBalanceBefore).sub(amount));
-
         });
 
 
         it("Should try exiting before the end of locking period", async () => {
-            const _codeName = "Convex: FRAX/USDC";
-            const _strategyAddress = strategy.address;
-            const _entryToken = usdcToken;
-            const _assetId = 0;
-            const _chainId = 1;
             const _entryData = await strategy.encodeEntryParams(
                 curvePool, _entryToken, poolSize, tokenIndexInCurve, fraxPool, duration);
             const _rewardsData = await strategy.encodeRewardsParams(lpToken, fraxPool, 0);
             const _exitData = await strategy.encodeExitParams(curvePool, _entryToken, tokenIndexInCurve, fraxPool);
-            const handler = await ethers.getContractAt("IStrategyHandler", "0x385AB598E7DBF09951ba097741d2Fa573bDe94A5");
-            const executor = await ethers.getContractAt("IVoteExecutorMaster", "0x82e568C482dF2C833dab0D38DeB9fb01777A9e89");
-            const poolToken = await ethers.getContractAt("IERC20Metadata", _entryToken);
-            const amount = parseUnits("1000", await poolToken.decimals());
 
             await handler.addLiquidityDirection(
                 _codeName,
-                _strategyAddress,
+                strategy.address,
                 _entryToken,
                 _assetId,
                 _chainId,
@@ -245,7 +200,6 @@ describe("Automated strategy execution", function () {
 
 
             const executorBalanceBefore = await poolToken.balanceOf(executor.address);
-            // await poolToken.connect(signer).transfer(executor.address, amount);
             const request1 = await executor.callStatic.encodeLiquidityCommand(_codeName, 6000);
             const request2 = await executor.callStatic.encodeLiquidityCommand("Curve/Convex Mim+3CRV", 4000);
             const request5 = await executor.callStatic.encodeLiquidityCommand("Curve/Convex Frax+USDC", 0);
@@ -260,7 +214,6 @@ describe("Automated strategy execution", function () {
             await executor.connect(admin).setMinSigns(0);
 
             await executor.executeSpecificData(3);
-            console.log('\n************* executed specific data *************\n');
             const executorBalanceAter = await poolToken.balanceOf(executor.address);
             console.log('Balance of executor before investing', ethers.utils.formatUnits(executorBalanceBefore, 6), "balance after withdrawal: ", ethers.utils.formatUnits(executorBalanceAter, 6), '\n**********************************');
 
@@ -269,7 +222,6 @@ describe("Automated strategy execution", function () {
 
             // try to exit before 7 days end
             await executor.submitData(inputData);
-            console.log('data submitted');
             const tx = executor.executeSpecificData(4);
             expect(tx).to.be.reverted;
 
@@ -287,43 +239,43 @@ describe("Automated strategy execution", function () {
         const poolSize = 2;
         const lpToken = '0xf43211935C781D5ca1a41d2041F397B8A7366C7A';
         const tokenIndexInCurve = 0;
+        const _codeName = "Convex: ETH/frxETH";
+        const _entryToken = wethToken;
+        const _assetId = 2;
+        const _chainId = 1;
 
         beforeEach(async () => {
 
             await resetNetwork();
 
-            const Strategy = await ethers.getContractFactory("CurveConvexStrategyNativeV2");
+            const Strategy = await ethers.getContractFactory("CurveFraxConvexStrategyV2");
             const routerAddress = "0x24733D6EBdF1DA157d2A491149e316830443FC00"
-            strategyNative = await upgrades.deployProxy(Strategy,
+            strategy = await upgrades.deployProxy(Strategy,
                 [signers[0].address, ZERO_ADDR, ZERO_ADDR, routerAddress], {
                 initializer: 'initialize',
                 kind: 'uups',
                 unsafeAllow: ["delegatecall"]
             }
-            ) as CurveConvexStrategyNativeV2
+            ) as CurveFraxConvexStrategyV2
 
-            await strategyNative.grantRole("0x0000000000000000000000000000000000000000000000000000000000000000", "0x82e568c482df2c833dab0d38deb9fb01777a9e89");
+            await strategy.grantRole("0x0000000000000000000000000000000000000000000000000000000000000000", "0x82e568c482df2c833dab0d38deb9fb01777a9e89");
+            handler = await ethers.getContractAt("IStrategyHandler", "0x385AB598E7DBF09951ba097741d2Fa573bDe94A5");
+            executor = await ethers.getContractAt("IVoteExecutorMaster", "0x82e568C482dF2C833dab0D38DeB9fb01777A9e89");
+            poolToken = await ethers.getContractAt("IERC20Metadata", wethToken);
 
         });
 
         it("Should invest into ETH/frxETH pool", async () => {
-            const _codeName = "Convex: ETH/frxETH";
-            const _strategyAddress = strategyNative.address;
-            const _entryToken = wethToken;
-            const _assetId = 2;
-            const _chainId = 1;
-            const _entryData = await strategyNative.encodeEntryParams(
+
+            const _entryData = await strategy.encodeEntryParams(
                 curvePool, _entryToken, poolSize, tokenIndexInCurve, fraxPool, duration);
-            const _rewardsData = await strategyNative.encodeRewardsParams(lpToken, fraxPool, 0);
-            const _exitData = await strategyNative.encodeExitParams(curvePool, _entryToken, tokenIndexInCurve, fraxPool);
-            const handler = await ethers.getContractAt("IStrategyHandler", "0x385AB598E7DBF09951ba097741d2Fa573bDe94A5");
-            const executor = await ethers.getContractAt("IVoteExecutorMaster", "0x82e568C482dF2C833dab0D38DeB9fb01777A9e89");
-            const poolToken = await ethers.getContractAt("IERC20Metadata", wethToken);
+            const _rewardsData = await strategy.encodeRewardsParams(lpToken, fraxPool, 0);
+            const _exitData = await strategy.encodeExitParams(curvePool, _entryToken, tokenIndexInCurve, fraxPool);
             const amount = parseUnits("100", await poolToken.decimals());
 
             await handler.addLiquidityDirection(
                 _codeName,
-                _strategyAddress,
+                strategy.address,
                 _entryToken,
                 _assetId,
                 _chainId,
@@ -334,8 +286,6 @@ describe("Automated strategy execution", function () {
 
 
             const executorBalanceBefore = await poolToken.balanceOf(executor.address);
-            console.log(executorBalanceBefore);
-            // await poolToken.connect(signer).transfer(executor.address, amount);
             const rq1 = await executor.callStatic.encodeLiquidityCommand(_codeName, 6000);
             const rq2 = await executor.callStatic.encodeLiquidityCommand("Curve/Convex Mim+3CRV", 4000);
             const rq3 = await executor.callStatic.encodeLiquidityCommand("Curve/Convex Mim+3CRV", 0);
@@ -344,14 +294,11 @@ describe("Automated strategy execution", function () {
             const encodedMmessages = await executor.callStatic.encodeAllMessages([rq1[0], rq2[0], rq3[0], rq4[0]], [rq1[1], rq2[1], rq3[1], rq4[1]]);
             const inputData = encodedMmessages[2];
             await executor.submitData(inputData);
-            console.log('here');
 
             const admin = await getImpersonatedSigner('0x1F020A4943EB57cd3b2213A66b355CB662Ea43C3');
             await executor.connect(admin).setMinSigns(0);
 
             await executor.executeSpecificData(3);
-            console.log('\n************* executed specific data *************\n');
-
             await poolToken.transfer(executor.address, amount);
             const executorBalanceAter = await poolToken.balanceOf(executor.address);
 
@@ -359,7 +306,6 @@ describe("Automated strategy execution", function () {
                 ethers.utils.formatEther(executorBalanceBefore), "balance after withdrawal: ",
                 ethers.utils.formatEther(executorBalanceAter),
                 '\n**********************************');
-            console.log(await poolToken.balanceOf(strategyNative.address));
             await executor.connect(admin).executeDeposits();
             console.log('\nDeposit executed!\n');
         });
